cf503b2049b6d289b1f40dbd25d3a098c813d6b6
k2o333 <790026345@qq,com>
2026-03-12 00:35:28 +0800

    first

---
+++ quantaalpha/contrib/model/experiment.py
@@ -0,0 +1,7 @@
+"""
+AlphaAgent compatibility layer: model_experiment.
+Re-exports from RD-Agent model_experiment for compatibility.
+"""
+
+from rdagent.scenarios.qlib.experiment.model_experiment import *  # type: ignore  # noqa: F401,F403
+
